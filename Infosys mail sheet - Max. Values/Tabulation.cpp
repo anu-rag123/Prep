b92676c63a96a33f384c3a2db539e3b9e6e6dc57
Anurag Khanna <khannaa395@gmail.com>
2022-07-26 11:57:53 +0530

    Update Tabulation.cpp

--- Infosys mail sheet - Max. Values/Tabulation.cpp
+++ Infosys mail sheet - Max. Values/Tabulation.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main(){
-    int n, m;
+    int n, m, mod = 1e9 + 7;
     cin>>n>>m;
     vector<vector<int>> grid(n, vector<int>(m));
     for(int i = 0; i < n; i++){
@@ -11,21 +11,21 @@ int main(){
         }
     }
     vector<vector<int>> dp(n, vector<int> (2, 0));
-    dp[n - 1][0] = grid[n - 1][0];
-    dp[n - 1][1] = grid[n - 1][1];
+    dp[n - 1][0] = grid[n - 1][0] % mod;
+    dp[n - 1][1] = grid[n - 1][1] % mod;
     for(int i = n - 2; i >= 0; i--){
         for(int j = 0; j < 2; j++){
             if(grid[i + 1][0] > grid[i][j] && grid[i + 1][1] > grid[i][j]){
-                dp[i][j] = max(grid[i][j] + dp[i + 1][0], grid[i][j] + dp[i + 1][1]);
+                dp[i][j] = max(grid[i][j] + dp[i + 1][0], grid[i][j] + dp[i + 1][1]) % mod;
             }
             else if(grid[i + 1][0] > grid[i][j]){
-                dp[i][j] = grid[i][j] + dp[i + 1][0];
+                dp[i][j] = (grid[i][j] + dp[i + 1][0]) % mod;
             }
             else if(grid[i + 1][1] > grid[i][j]){
-                dp[i][j] = grid[i][j] + dp[i + 1][1];
+                dp[i][j] = (grid[i][j] + dp[i + 1][1]) % mod;
             }
             else{
-                dp[i][j] = grid[i][j];
+                dp[i][j] = grid[i][j] % mod;
             }
         }
     }
